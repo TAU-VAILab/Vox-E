4eb01a424046263f2d050778f4fb5495980f43eb
galf-fly <gal@flymingo.net>
2023-02-28 16:07:59 +0200

    merge push 1

--- render_sh_based_voxel_grid_attn.py
+++ render_sh_based_voxel_grid_attn.py
@@ -3,17 +3,20 @@ import click
 import imageio
 import torch
 
+from thre3d_atom.thre3d_reprs.sd_attn import StableDiffusion
 from thre3d_atom.modules.volumetric_model import (
-    create_volumetric_model_from_saved_model,
+    create_volumetric_model_from_saved_model, create_volumetric_model_from_saved_model_attn
 )
-from thre3d_atom.thre3d_reprs.voxels import create_voxel_grid_from_saved_info_dict
+from thre3d_atom.thre3d_reprs.voxels import create_voxel_grid_from_saved_info_dict, \
+    create_voxel_grid_from_saved_info_dict_attn
 from thre3d_atom.utils.constants import HEMISPHERICAL_RADIUS, CAMERA_INTRINSICS
 from thre3d_atom.utils.imaging_utils import (
     get_thre360_animation_poses,
     get_thre360_spiral_animation_poses,
 )
 from thre3d_atom.visualizations.animations import (
-    render_camera_path_for_volumetric_model,
+    render_camera_path_for_volumetric_model, render_camera_path_for_volumetric_model_with_attention_and_diffusion,
+    render_camera_path_for_volumetric_model_attn
 )
 from easydict import EasyDict
 
@@ -31,9 +34,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
               required=True, help="path to the trained (reconstructed) model")
 @click.option("-o", "--output_path", type=click.Path(file_okay=False, dir_okay=True),
               required=True, help="path for saving rendered output")
-@click.option("-r", "--ref_path", type=click.Path(file_okay=True, dir_okay=False), default=None,
-              required=False, help="path for saving rendered output")
-
 # Non-required Render configuration options:
 @click.option("--overridden_num_samples_per_ray", type=click.IntRange(min=1), default=512,
               required=False, help="overridden (increased) num_samples_per_ray for beautiful renders :)")
@@ -51,16 +51,21 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
               required=False, help="height at which the camera spiralling will happen")
 @click.option("--num_spiral_rounds", type=click.IntRange(min=1), default=2,
               required=False, help="number of rounds made while transitioning between spiral radii")
-
 # Non-required video options:
 @click.option("--fps", type=click.IntRange(min=1), default=60,
               required=False, help="frames per second of the video")
+@click.option("--timestamp", type=click.INT, default=0,
+              required=False, help="diffusion_timestamp")
+@click.option("--use_sd", type=click.BOOL, default=False,
+              required=False, help="render with stable diffusion")
+@click.option("--load_attention", type=click.BOOL, default=False,
+              required=False, help="render with attention features")
+@click.option("--prompt", type=click.STRING, required=False, default='',
+              help="prompt for attention focus")
+@click.option("--index_to_attn", type=click.INT, required=False, default=11,
+              help="index to apply attention to", show_default=True)
 
-# Output saving additions:
-@click.option("--save_freq", type=click.INT, default=None,
-              required=False, help="frames per second of the video")
-@click.option("-p", "--sds_prompt", type=click.STRING, required=False, default=None,
-              help="sds prompt used for SDS based loss, if not None, prints it out to a file")
+
 
 # fmt: on
 # -------------------------------------------------------------------------------------
@@ -71,34 +76,25 @@ def main(**kwargs) -> None:
     # parse os-checked path-strings into Pathlike Paths :)
     model_path = Path(config.model_path)
     output_path = Path(config.output_path)
-
+    sd_model = None
+    if config.use_sd:
+        sd_model = StableDiffusion(device, "1.4")
     # create the output path if it doesn't exist
     output_path.mkdir(exist_ok=True, parents=True)
 
-    # save prompt to text file if not None
-    if config.sds_prompt != None:
-        text_path = output_path / "prompt.txt"
-        with open(text_path, 'w') as file:
-            file.write(config.sds_prompt)
-
-    # load volumetric_model from the model_path
-    vol_mod, extra_info = create_volumetric_model_from_saved_model(
-        model_path=model_path,
-        thre3d_repr_creator=create_voxel_grid_from_saved_info_dict,
-        device=device,
-    )
-    vol_mod.render_config.random_bkgd = False
-    vol_mod.render_config.white_bkgd = True
-
-    # override extra info with ref's if given - raises quality
-    if config.ref_path != None:
-        ref_path = Path(config.ref_path)
-        _, extra_info_ref = create_volumetric_model_from_saved_model(
-            model_path=ref_path,
-            thre3d_repr_creator=create_voxel_grid_from_saved_info_dict,
-            device=device,
+    if config.load_attention:
+        vol_mod, extra_info = create_volumetric_model_from_saved_model_attn(
+            model_path=model_path,
+            thre3d_repr_creator=create_voxel_grid_from_saved_info_dict_attn,
+            device=device, load_attn=config.load_attention
         )
-        extra_info = extra_info_ref
+    # load volumetric_model from the model_path
+    else:
+        vol_mod, extra_info = create_volumetric_model_from_saved_model(
+            model_path=model_path,
+            thre3d_repr_creator=create_voxel_grid_from_saved_info_dict,
+            device=device
+        )
 
     hemispherical_radius = extra_info[HEMISPHERICAL_RADIUS]
     camera_intrinsics = extra_info[CAMERA_INTRINSICS]
@@ -128,15 +124,39 @@ def main(**kwargs) -> None:
             f"Only available options are: ['thre360' and 'spiral']"
         )
 
-    animation_frames = render_camera_path_for_volumetric_model(
-        vol_mod=vol_mod,
-        camera_path=animation_poses,
-        camera_intrinsics=camera_intrinsics,
-        overridden_num_samples_per_ray=config.overridden_num_samples_per_ray,
-        render_scale_factor=config.render_scale_factor,
-        image_save_freq=config.save_freq,
-        image_save_path=output_path,
-    )
+    if config.load_attention:
+        if config.use_sd:
+            animation_frames, attn = render_camera_path_for_volumetric_model_with_attention_and_diffusion(
+                vol_mod=vol_mod,
+                camera_path=animation_poses,
+                camera_intrinsics=camera_intrinsics,
+                sd_model=sd_model,
+                device=device,
+                prompt=config.prompt,
+                index_to_attn=[config.index_to_attn],
+                overridden_num_samples_per_ray=config.overridden_num_samples_per_ray,
+                render_scale_factor=config.render_scale_factor,
+                timestamp=config.timestamp
+            )
+        else:
+            animation_frames, attn = render_camera_path_for_volumetric_model_attn(
+                vol_mod=vol_mod,
+                camera_path=animation_poses,
+                camera_intrinsics=camera_intrinsics,
+                overridden_num_samples_per_ray=config.overridden_num_samples_per_ray,
+                render_scale_factor=config.render_scale_factor
+            )
+        for i, f in enumerate(attn):
+            imageio.imwrite(output_path / "{}.png".format(i), f)
+
+    else:
+        animation_frames = render_camera_path_for_volumetric_model(
+            vol_mod=vol_mod,
+            camera_path=animation_poses,
+            camera_intrinsics=camera_intrinsics,
+            overridden_num_samples_per_ray=config.overridden_num_samples_per_ray,
+            render_scale_factor=config.render_scale_factor
+        )
 
     imageio.mimwrite(
         output_path / "rendered_video.mp4",
